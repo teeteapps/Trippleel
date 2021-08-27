2d4cda96fce87e536ff30caa4794983389f4f535
Francis Kingori <francis.kingori@tovutigroup.com>
2021-08-27 19:41:28 +0300

    update

---
+++ Trippleel/Controllers/AccountsController.cs
@@ -0,0 +1,26 @@
+﻿using DBL;
+using DBL.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trippleel.Controllers
+{
+    [Authorize]
+    public class AccountsController : BaseController
+    {
+        private readonly BL bl;
+        EncryptDecrypt sec = new EncryptDecrypt();
+        public AccountsController()
+        {
+            bl = new BL(Util.ShareConnectionString.Value);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
