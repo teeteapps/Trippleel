acb779245199b6aa582e5caa89ffb6f170669a66
Francis Kingori <francis.kingori@tovutigroup.com>
2021-08-28 14:20:00 +0300

    update

---
+++ Trippleel/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using DBL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trippleel.Controllers
+{
+    [Authorize]
+    public class ProductsController : BaseController
+    {
+        private readonly BL bl;
+        public ProductsController()
+        {
+            bl = new BL(Util.ShareConnectionString.Value);
+        }
+        public IActionResult Productlist()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Addproduct()
+        {
+            return PartialView("_Productspartial");
+        }
+    }
+}
---
+++ Trippleel/wwwroot/js/products.js
@@ -0,0 +1,11 @@
+﻿$('#producdatamodal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var url = button.attr("href");
+    var modal = $(this);
+    modal.find('.modal-content').load(url);
+});
+$('#producdatamodal').on('hidden.bs.modal', function () {
+    $(this).removeData('bs.modal');
+    $('#producdatamodal .modal-content').empty();
+});
+
