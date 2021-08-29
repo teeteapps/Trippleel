2863f5c64e1fef6b131d3cf5c22e13117d369990
Francis Kingori <francis.kingori@tovutigroup.com>
2021-08-29 19:55:22 +0300

    update

--- Trippleel/wwwroot/js/products.js
+++ Trippleel/wwwroot/js/products.js
@@ -2,16 +2,15 @@
     var button = $(event.relatedTarget);
     var url = button.attr("href");
     var modal = $(this);
-    modal.find('.modalcontentdata').load(url);
+    modal.find('.modal-content').load(url);
 });
 $('#producdatamodal').on('hidden.bs.modal', function () {
     $(this).removeData('bs.modal');
-    $('#producdatamodal .modalcontentdata').empty();
+    $('#producdatamodal .modal-content').empty();
 });
 
 function loadproductsubcategory() {
     $("#prodsubcategoryId").empty();
-
     $.ajax({
         type: "GET",
         url: "GetListModelbycode",
